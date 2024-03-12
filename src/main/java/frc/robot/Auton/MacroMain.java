656e6b54699a53423c0d0b2f85042bd7d1285442
TTheHuman <Miket5279@gmail.com>
2024-03-12 16:31:19 -0400

    done(?); God still dead?????????

--- src/main/java/frc/robot/Auton/MacroMain.java
+++ src/main/java/frc/robot/Auton/MacroMain.java
@@ -1,16 +1,90 @@
 package frc.robot.Auton;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
 
-public class MacroMain{
-    Scanner scanner;
-    long startTime;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.XboxController;
 
-    boolean OnTime = true;
-    double nextDouble;
-    public MacroMain() throws FileNotFoundException{
-        scanner = new Scanner();
+public class MacroMain extends Command{
+    boolean isworking = false;
+    boolean isoff = false;
+    boolean running = false;
+    boolean isRecording = false;
+
+
+    static final int autonumber = 1;
+
+    private final Timer timer = new Timer();
+
+    MacroPlayer player;
+
+    MacroRecorder recorder;
+
+    XboxController controller0 = new XboxController(0);
+
+    static final String autoFile = new String("/java/src/robot/Auton" + autonumber + ".csv");
+
+    public MacroMain(){
+        try {
+            player = new MacroPlayer(null, null, null, null);
+        } catch (FileNotFoundException e) {
+            
+            e.printStackTrace();
+        }
     }
+    
+    public void autonomous(){
+        MacroPlayer player = null;
+    }
+    public void execute(){
+        while(isoff == false){
+            if(player != null){
+                player.play();
+            }
+
+        }
+        if(player != null){
+            player.end();
+        }
+    }
+    public void Control(){
+
+            try{
+                recorder = new MacroRecorder();
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        while(isworking){
+            if(controller0.getAButton()){
+                isRecording = !isRecording;
+            }
+            if(isRecording){
+                try {
+                    if(isRecording){
+                        recorder.Record();
+                    }
+                } 
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        try {
+            if(recorder != null){
+                recorder.end();
+            }
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void end(boolean interrupted) {}
+    
+    public boolean isFinished() {
+        return false;
+      }
 }
---
+++ src/main/java/frc/robot/Auton/MacroPlayer.java
@@ -0,0 +1,82 @@
+package frc.robot.Auton;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+import frc.robot.subsystems.DriveTrainSubsystem;
+
+public class MacroPlayer{
+    Scanner scanner;
+    long startTime;
+
+    DriveTrainSubsystem leftMotor;
+    DriveTrainSubsystem leftMotor2;
+    DriveTrainSubsystem rightMotor;
+    DriveTrainSubsystem rightMotor2;
+
+
+    double xSpeed;
+    double zSpeed;
+
+
+    boolean OnTime = true;
+    double nextDouble;
+    public MacroPlayer(DriveTrainSubsystem leftMotor, DriveTrainSubsystem rightMotor, DriveTrainSubsystem leftMotor2, DriveTrainSubsystem rightMotor2) throws FileNotFoundException{
+
+        this.leftMotor = leftMotor;
+        this.leftMotor2 = leftMotor2;
+        this.rightMotor = rightMotor;
+        this.rightMotor2 = rightMotor2;
+
+
+
+
+
+        scanner = new Scanner(new File (MacroMain.autoFile));
+
+        scanner.useDelimiter(",|\\n");
+
+        startTime = System.currentTimeMillis();
+    }
+
+    public void play(){
+        if((scanner) != null && (scanner.hasNextDouble())){
+            double t_delta;
+
+            if(OnTime){
+                nextDouble = scanner.nextDouble();
+            }
+
+            t_delta = nextDouble - (System.currentTimeMillis()-startTime);
+
+            if (t_delta <= 0){
+
+                leftMotor.arcadeDrive(nextDouble, nextDouble);
+                leftMotor2.arcadeDrive(nextDouble, nextDouble);
+                rightMotor.arcadeDrive(nextDouble, nextDouble);
+                rightMotor.arcadeDrive(nextDouble, nextDouble);
+                OnTime = true;
+            }
+
+            else{
+                OnTime = false;
+            }
+        }
+        else{
+            if(scanner != null){
+                scanner.close();
+                scanner = null;
+            }
+        }
+    }
+    public void end(){
+        leftMotor.arcadeDrive(0, 0);
+        leftMotor2.arcadeDrive(0, 0);
+        rightMotor.arcadeDrive(0, 0);
+        rightMotor.arcadeDrive(0, 0);
+        if(scanner != null){
+            scanner.close();
+        }
+    }
+}
