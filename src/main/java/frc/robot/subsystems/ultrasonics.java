627553b066f3e687a4f0fb7b49d95394e0de862f
5DHD <natertot11@icloud.com>
2024-05-15 16:08:40 -0400

    sensor hehehe

---
+++ src/main/java/frc/robot/subsystems/ultrasonics.java
@@ -0,0 +1,22 @@
+import edu.wpi.first.math.filter.MedianFilter;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.Ultrasonic;
+
+public class ultrasonics {
+    static final int kUltrasonicPingPort = 0;
+    static final int kUltrasonicEchoPort = 1;
+
+    // Ultrasonic sensors tend to be quite noisy and susceptible to sudden outliers,
+    // so measurements are filtered with a 5-sample median filter
+    private final static MedianFilter m_filter = new MedianFilter(5);
+
+    private final static Ultrasonic m_ultrasonic = new Ultrasonic(kUltrasonicPingPort, kUltrasonicEchoPort);
+
+    static double GetMeasurement(){
+    //get measurement and filter it
+    double measurement = m_ultrasonic.getRangeMM();
+    double filteredMeasurement = m_filter.calculate(measurement);
+    return filteredMeasurement;
+    }
+}
